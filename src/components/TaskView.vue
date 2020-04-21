7c3f8553512c656d13e90e21bfcb5a5a110097f1
Matthew Beaumont <matt@beaumontwebdev.com>
2020-04-21 01:57:16 -0400

    moved task list to appropriate view

--- src/components/TaskView.vue
+++ src/components/TaskView.vue
@@ -1,7 +1,59 @@
-<template> <div></div></template>
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-data-table
+          :items="tasks"
+          :headers="headers"
+          show-select
+          sort-by="project"
+          :items-per-page="20"
+          item-key="name"
+          v-model="selected"
+        >
+          <template v-slot:item.time="{ item }">
+            {{ item.time | clickupHours }}
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </v-container></template
+>
 
 <script>
 export default {
+  computed: {
+    tasks() {
+      return this.$store.state.tasks.map(task => {
+        return {
+          id: task.id,
+          project: task.folder.name,
+          name: task.name,
+          time: task.time_spent
+        };
+      });
+    }
+  },
+  data() {
+    return {
+      selected: [],
+      headers: [
+        {
+          text: "Project Name",
+          value: "project",
+          align: "start"
+        },
+        {
+          text: "Task Name",
+          value: "name"
+        },
+        {
+          text: "Time",
+          value: "time"
+        }
+      ]
+    };
+  },
   name: "TaskView"
 };
 </script>
