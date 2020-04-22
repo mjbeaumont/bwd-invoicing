9823bfb65570542d2ac87360efce9c4ee803bde6
Matthew Beaumont <matt@beaumontwebdev.com>
2020-04-21 20:31:29 -0400

    fixed bug where Ripple wasn't loaded

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import { preset } from "vue-cli-plugin-vuetify-preset-rally/preset";
+import { Ripple } from "vuetify/lib/directives";
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  directives: {
+    Ripple
+  }
+});
 
 export default new Vuetify({
   preset
